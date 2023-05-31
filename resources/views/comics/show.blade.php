5c56345f73ea877402177e5ae31ba11efbd7da18
totarochristian <totaro.christian.18@gmail.com>
2023-05-31 11:27:13 +0200

    Update comics show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,19 +3,19 @@
 @section('content')
     <section class="container showDetailGroup">
         <h1>{{ $comic->title }}</h1>
-        <div class="card">
-            <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
-            <div class="card-body">
-                <h5 class="card-title"> {{ $comic->title }}</h5>
-                <p class="card-text">{!! $comic->description !!}</p>
-                <p class="card-text"><strong>Prezzo ($):</strong> {{ $comic->price }}</p>
-                <p class="card-text"><strong>Serie:</strong> {{ $comic->series }}</p>
-                <p class="card-text"><strong>Tipologia:</strong> {{ $comic->type }}</p>
-                <p class="card-text"><strong>Data di uscita:</strong> {{ $comic->sale_date }}</p>
+        <div class="d-flex gap-4 mb-4">
+            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+            <div class="d-flex flex-column">
+                <h3> {{ $comic->title }}</h3>
+                <p>{!! $comic->description !!}</p>
+                <p><strong>Prezzo ($):</strong> {{ $comic->price }}</p>
+                <p><strong>Serie:</strong> {{ $comic->series }}</p>
+                <p><strong>Tipologia:</strong> {{ $comic->type }}</p>
+                <p><strong>Data di uscita:</strong> {{ $comic->sale_date }}</p>
             </div>
         </div>
         @if(count($artists)>0)
-            <h1>Artisti</h1>
+            <h2>Artisti</h2>
             <div class="row gy-4">
                 @foreach ($artists as $artist)
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3">
@@ -29,7 +29,7 @@
             </div>
         @endif
         @if(count($writers)>0)
-            <h1>Scrittori</h1>
+            <h2 class="mt-4">Scrittori</h2>
             <div class="row gy-4">
                 @foreach ($writers as $writer)
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3">
