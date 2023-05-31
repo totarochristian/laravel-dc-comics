e8babf6b0b982b80e022d29e9dac6b53f580045a
totarochristian <totaro.christian.18@gmail.com>
2023-05-31 12:28:07 +0200

    Manage the validation in the edit page of comics

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -8,35 +8,56 @@
             @method('PUT')
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" name="title" id="title" aria-describedby="titleHelp" value="{{ $comic->title }}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" aria-describedby="titleHelp" value="{{ old('title',$comic->title) }}">
                 <div id="titleHelp" class="form-text">Inserisci il titolo del fumetto</div>
+                @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea name="description" id="description" cols="30" rows="10">{{ $comic->description }}</textarea>
+                <textarea name="description" class="@error('description') is-invalid @enderror" id="description" cols="30" rows="10">{{ old('description',$comic->description) }}</textarea>
+                @error('description')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             <div class="mb-3">
                 <label for="thumb" class="form-label">Immagine thumb</label>
-                <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="thumbHelp" value="{{ $comic->thumb }}">
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" id="thumb" aria-describedby="thumbHelp" value="{{ old('thumb',$comic->thumb) }}">
                 <div id="thumbHelp" class="form-text">Inserisci l'url all'immagine del fumetto</div>
+                @error('thumb')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="number" step="0.01" class="form-control" name="price" id="price" aria-describedby="priceHelp" value="{{ $comic->price }}">
+                <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" name="price" id="price" aria-describedby="priceHelp" value="{{ old('price',$comic->price) }}">
                 <div id="priceHelp" class="form-text">Inserisci il prezzo del fumetto</div>
+                @error('price')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control" name="series" id="series" aria-describedby="seriesHelp" value="{{ $comic->series }}">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" id="series" aria-describedby="seriesHelp" value="{{ old('series',$comic->series) }}">
                 <div id="seriesHelp" class="form-text">Inserisci la serie del fumetto</div>
+                @error('series')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="sale_date">Start date:</label>
-                <input type="date" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+                <label for="sale_date">Data d'uscita:</label>
+                <input type="date" class="@error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{ old('sale_date',$comic->sale_date) }}">
+                @error('sale_date')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipologia</label>
-                <input type="text" class="form-control" name="type" id="type" aria-describedby="typeHelp" value="{{ $comic->type }}">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type" aria-describedby="typeHelp" value="{{ old('type',$comic->type) }}">
                 <div id="typeHelp" class="form-text">Inserisci la tipologia del fumetto</div>
+                @error('type')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <button type="submit" class="btn btn-primary">Modifica</button>
